cca0a865ea6b93d89f0df00f13528c1ff7ea5f84
Richard Shade <rshade@rightscale.com>
2015-02-23 17:21:04 -0500

    adding base files

---
+++ Gemfile
@@ -0,0 +1,11 @@
+  source 'https://rubygems.org'
+
+  gem 'praxis'
+  gem 'rack', '~> 1.0'
+  gem 'rake'
+  gem 'json'
+  gem 'fog'
+
+  group :development, :test do
+    gem 'rspec'
+  end
---
+++ design/resources/hello.rb
@@ -0,0 +1,36 @@
+  module V1
+    module ApiResources
+      class Hello
+        include Praxis::ResourceDefinition
+  
+        media_type V1::MediaTypes::Hello
+        version '1.0'
+  
+        routing do
+          prefix '/api/hello'
+        end
+  
+        action :index do
+          use :versionable
+  
+          routing do
+            get ''
+          end
+          response :ok
+        end
+  
+        action :show do
+          use :versionable
+  
+          routing do
+            get '/:id'
+          end
+          params do
+            attribute :id, Integer, required: true, min: 0
+          end
+          response :ok
+          response :not_found
+        end
+      end
+    end
+  end
