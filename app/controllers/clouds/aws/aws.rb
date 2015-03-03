fd593fc08acbf3b4cdd3b495e45aa959fb6cec1c
Richard Shade <rshade@rightscale.com>
2015-03-02 21:57:18 -0600

    fixing class

--- app/controllers/clouds/aws/aws.rb
+++ app/controllers/clouds/aws/aws.rb
@@ -2,8 +2,8 @@ module V1
   class Clouds
     class AWS
       include Praxis::Controller
-
       implements V1::ApiResources::Clouds::AWS
+      
       AVAILABLE_METHODS=["elb"]
       def index(**params)
         response.headers['Content-Type'] = 'application/json'
